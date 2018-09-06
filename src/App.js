88c7a1cc83771fb250d3e18311dabdc818118fc4
zuixin <zuixin@pinduoduo.com>
2018-09-06 19:00:36 +0800

    feat: init

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Tree } from 'g6-for-react';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: {}
+        }
+    };
+
+    componentDidMount() {
+        const data = {
+            roots: [{
+                label: '根部',
+                children: [{
+                    label: '子1',
+                    children: [
+                        {
+                            label: '子1.1'
+                        }
+                    ]
+                }, {
+                    label: '子2'
+                }]
+            }]
+        };
+        this.setState({
+            data: data
+        })
+    }
+
+    render() {
+        const data = this.state.data;
+        if (JSON.stringify(data) === '{}') {
+            return '';
+        }
+        return (
+            <div className="App">
+                <Tree fitView="cc" animate={true} height={window.innerHeight} data={data}></Tree>
+            </div>
+        );
+    }
+}
+
+export default App;
